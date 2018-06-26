9f6e70240093e89062d9375fbf9a24c1677d0597
Monty Anderson <montydev@protonmail.com>
2018-06-26 13:56:12 +0100

    Scalable + Stores History

--- server.js
+++ server.js
@@ -2,14 +2,21 @@ const Redis = require('ioredis');
 const socketIo = require('socket.io');
 const dogNames = require('dog-names');
 
-const redis = new Redis();
+const pub = new Redis();
+const sub = new Redis();
+
+const messagesChannel = 'chat-messages';
+const chatHistory = 'chat-history';
+
+sub.subscribe(messagesChannel);
+
 const io = socketIo(3050);
 
 const domains = {};
 
 io.on('connection', socket => {
 	socket.on('init', domain => {
-		if (!(domain in domains))
+		if (!(domains[domain] instanceof Set))
 			domains[domain] = new Set();
 
 		domains[domain].add(socket);
@@ -18,12 +25,34 @@ io.on('connection', socket => {
 
 		socket.emit('name', name);
 
-		socket.on('message', text => {
-			for (const socket of domains[domain])
-				socket.emit('message', {
+		socket.on('message', async text => {
+			await pub.publish(messagesChannel, JSON.stringify({
+				message: {
 					name,
 					text
-				});
+				},
+				domain
+			}));
+
+			await redis.lpush(`${chatHistory}:${domain}`, JSON.stringify(message));
 		});
+
+		socket.on('disconnect', () => {
+			domains[domain].delete(socket);
+		});
+
+		const rawMessages = await redis.lrange(`${chatHistory}:${domain}`, 0, -1);
+
+		for (const rawMessage of rawMessages)
+			socket.emit('message', JSON.parse(rawMessage));
 	});
+});
+
+sub.on('message', (channel, rawMessage) => {
+	const {message, domain} = JSON.parse(rawMessage);
+
+	if (domains[domain] instanceof Set) {
+		for (const socket of domains[domain])
+			socket.emit('message', message);
+	}
 });
