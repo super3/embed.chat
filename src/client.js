cdebaf1a631fb22029af7321012fd8d147825d06
Monty Anderson <montydev@protonmail.com>
2018-07-02 12:01:50 +0100

    Scope all css rules to avoid conflict with page's resources

--- src/client.js
+++ src/client.js
@@ -1,136 +1,32 @@
-/* global document, window, location */
+/* global document, location */
 const fs = require('fs');
 const io = require('socket.io-client');
 
 document.addEventListener('DOMContentLoaded', () => {
-	document.head.innerHTML += `
-		<style>
-			.chat, .chat * {
-				margin: 0;
-				padding: 0;
-			}
-
-			.chat {
-				position: fixed;
-				font-size: 17px;
-
-				right: 25px;
-				bottom: 0px;
-
-				height: 400px;
-				width: 300px;
-
-				z-index: 1000;
-
-				font-family: sans-serif;
-
-				border: 1px solid #D8D8D8;
-				border-radius: 5px;
-				border-bottom-left-radius: 0px;
-				border-bottom-right-radius: 0px;
-
-				display:none;
-			}
-
-			.chat .messages {
-				overflow-y: scroll;
-				height: calc(90% - 65px);
-				padding-top: 10px;
-				padding-bottom: 10px;
-				border-bottom: 1px solid #D8D8D8;
-			}
-
-			.sendbox {
-				display: flex;
-				padding: 10px 15px 10px 15px;
-				background-color: #F7F7F7;
-			}
-
-			.chat .messages li {
-				margin: 5px 15px 0px 15px;
-				list-style-type: none;
-			}
-
-			.chat .name {
-				background-color: #E9ECEF;
-				padding: 10px;
-				border: 1px solid #ced4da;
-				border-right: 0px;
-				border-radius: 5px;
-				border-top-right-radius: 0px;
-				border-bottom-right-radius: 0px;
-				position: relative;
-				top: 1px;
-			}
-
-			.chat input {
-				height: 10%;
-				width:100%;
-
-				border-radius: 5px;
-				padding: 15px;
-				border: 1px solid #ced4da;
-
-				border-top-left-radius: 0px;
-				border-bottom-left-radius: 0px;
-			}
-
-			.sendname {
-				text-align: right;
-				padding-top: 10px;
-			}
-
-			.dot {
-				height: 60px;
-				width: 60px;
-				background-color: #1972F5;
-				border-radius: 50%;
-				display: inline-block;
-
-				position: fixed;
-				right: 24px;
-				bottom: 20px;
-
-				box-shadow: 3px 3px 20px #ccc;
-			}
-
-			.dot svg {
-				display: block;
-				filter: invert(100%);
-				height: 35px;
-				width: 35px;
-				margin: auto;
-				margin-top: 11px;
-			}
-
-			.x {
-				height: 20px;
-				background-color: #F7F7F7;
-				border-bottom: 1px solid #D8D8D8;
-				padding: 0px 10px 3px 3px;
-				text-align: right;
-			}
-		</style>
-	`;
+	document.head.innerHTML += `<style>${
+		fs.readFileSync(`${__dirname}/style.css`, 'utf8')
+	}</style>`;
 
 	const comments = fs.readFileSync(`${__dirname}/comments.svg`, 'utf8');
 
 	document.body.innerHTML += `
-		<span class="dot">
-			${comments}
-		</span>
+		<div class='__embed.chat'>
+			<span class="dot">
+				${comments}
+			</span>
 
-		<div class='chat'>
-			<div class='x'>x</div>
+			<div class='chat'>
+				<div class='x'>x</div>
 
-			<ul class='messages'></ul>
+				<ul class='messages'></ul>
 
-			<div class='sendbox'>
-				<div class='sendname'>
-					<span class='name'></span>
+				<div class='sendbox'>
+					<div class='sendname'>
+						<span class='name'></span>
+					</div>
+
+					<input placeholder="Type message here...">
 				</div>
-
-				<input placeholder="Type message here...">
 			</div>
 		</div>
 	`;
