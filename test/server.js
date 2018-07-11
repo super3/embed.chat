c59e2ab966dd384bf4feebf6343632d0b5de09ee
Monty Anderson <montydev@protonmail.com>
2018-07-11 11:03:26 +0100

    Basic Tests

--- test/server.js
+++ test/server.js
@@ -1,5 +1,34 @@
-/* global describe */
+/* global describe, it */
+const assert = require('assert');
+const io = require('socket.io-client');
+const server = require('../server');
+
+const socket = io('http://localhost:3050');
 
 describe('Server', () => {
+	it('should send stats', async () => {
+		socket.emit('subscribe-stats');
 
+		await new Promise(resolve => socket.on('stats', resolve));
+	});
+
+	let name;
+
+	it('should allocate name', async () => {
+		socket.emit('init', 'localhost', true);
+
+		name = await new Promise(resolve => socket.on('name', resolve));
+	});
+
+	it('should relay message', async () => {
+		socket.emit('message', 'Hello, World!');
+
+		assert.deepEqual(
+			await new Promise(resolve => socket.on('message', resolve)),
+			{
+				name,
+				text: 'Hello, World!'
+			}
+		);
+	});
 });
